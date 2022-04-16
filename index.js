83f8803ef44bf27164868fdff5fbc7ad3b8f5497
Box <j@box.lol>
2022-04-16 12:45:33 -0400

    Add presence intent

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Client, Intents } = require('discord.js');
 const client = new Client({
-    intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MEMBERS],
+    intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_PRESENCES],
 });
 
 const { promisify } = require('util');
