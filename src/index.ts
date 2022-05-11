9339e8b80faa79112dd5d34e533b51208696ba99
Box <j@box.lol>
2022-05-11 13:10:59 -0400

    Prevent reply mentions by default

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,51 @@
-import { Client, Collection, Intents, MessageEmbed } from 'discord.js';
-const client: any = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MEMBERS] });
+import { Client, Collection, Intents, MessageEmbed } from 'discord.js'
+const client: any = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES],
+    allowedMentions: { repliedUser: false },
+})
 
 // Basic imports
-import { exec } from 'child_process';
-import { readdirSync, statSync, readFile, writeFile } from 'fs';
-import readline from 'readline';
-import { join } from 'path';
-import mongoose from 'mongoose';
+import { exec } from 'child_process'
+import { readdirSync, statSync, readFile, writeFile } from 'fs'
+import readline from 'readline'
+import { join } from 'path'
+import mongoose from 'mongoose'
 
 // Prepare dotenv config
-import dotenv from 'dotenv';
-dotenv.config();
-const { TOKEN, MONGODB } = process.env;
+import dotenv from 'dotenv'
+dotenv.config()
+const { TOKEN, MONGODB } = process.env
 
 // appending to the client
-client.commands = new Collection();
-client.Database = import('./db/Mongo.js');
+client.commands = new Collection()
+client.Database = import('./db/Mongo.js')
 
 // main init function
 async function init() {
     // load events
-    const eventFiles = readdirSync('./events').filter((file) => file.endsWith('.js'));
+    const eventFiles = readdirSync('./events').filter((file) => file.endsWith('.js'))
     for (const file of eventFiles) {
-        const { default: event } = await import(`./events/${file}`);
+        const { default: event } = await import(`./events/${file}`)
         if (event.once) {
-            client.once(event.name, (...args) => event.execute(...args));
+            client.once(event.name, (...args) => event.execute(...args))
         } else {
-            client.on(event.name, (...args) => event.execute(...args));
+            client.on(event.name, (...args) => event.execute(...args))
         }
     }
 
     // grab command categories
     const commandFolders = readdirSync('./cmds')
         .filter((file) => statSync(join('./cmds', file)).isDirectory())
-        .filter((file) => !file.startsWith('_'));
+        .filter((file) => !file.startsWith('_'))
 
     // load commands
     for (const folder of commandFolders) {
         const commandFiles = readdirSync(`./cmds/${folder}`)
             .filter((file) => file.endsWith('.js'))
-            .filter((file) => !file.startsWith('_'));
+            .filter((file) => !file.startsWith('_'))
         for (const file of commandFiles) {
-            const { default: command } = await import(`./cmds/${folder}/${file}`);
-            client.commands.set(command.data.name, command);
+            const { default: command } = await import(`./cmds/${folder}/${file}`)
+            client.commands.set(command.data.name, command)
         }
     }
 
@@ -50,14 +53,14 @@ async function init() {
     await mongoose
         .connect(MONGODB)
         .then(() => {
-            console.log('Connected to database');
+            console.log('Connected to database')
         })
         .catch((err) => {
-            console.log(`Unable to connect to MongoDB Database.\nError: ${err}`);
-        });
+            console.log(`Unable to connect to MongoDB Database.\nError: ${err}`)
+        })
 
     // login
-    await client.login(TOKEN);
+    await client.login(TOKEN)
 }
 
-init();
+init()
